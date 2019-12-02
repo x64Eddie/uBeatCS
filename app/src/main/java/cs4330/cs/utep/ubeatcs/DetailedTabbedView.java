02c0361040d42905a155597e99dcdf25a485b98a
Eddie <eherrerag.eh@gmail.com>
2019-12-01 18:23:39 -0700

    Now the files that are selected from the gallery are going to be uploaded to the firebase storage

--- app/src/main/java/cs4330/cs/utep/ubeatcs/DetailedTabbedView.java
+++ app/src/main/java/cs4330/cs/utep/ubeatcs/DetailedTabbedView.java
@@ -1,18 +1,28 @@
 package cs4330.cs.utep.ubeatcs;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -48,21 +58,23 @@ public class DetailedTabbedView extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == LOADING_IMAGE_RESULT){
-            try{
+        if(requestCode == LOADING_IMAGE_RESULT){
                 final Uri imageUri = data.getData();
-                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                configureImageDest(selectedImage);
-            }catch(FileNotFoundException e){
-                e.printStackTrace();
-            }
+//                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+//                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                configureImageDest(imageUri);
+
         }
     }
 
-    private void configureImageDest(Bitmap image){
-        //TODO display the dialog to the user that is selecting the destination of the bitmap, then ->
-        //TODO send image to model-layer or activity that is going to save the image to display in the imageView
+    private void configureImageDest(Uri image){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        FirebaseStorage.getInstance("gs://ubeatcs.appspot.com").getReference("UserUpload/"+image.hashCode()).putFile(image).addOnFailureListener(
+                e -> Toast.makeText(getApplicationContext(), "Upload Failed", Toast.LENGTH_LONG).show()).addOnSuccessListener(
+                        taskSnapshot -> Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show()).addOnProgressListener(taskSnapshot -> {
+                            double progress = (100 * taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage(((int)progress)+ "% Uploaded... ");
+                        });
     }
 
     private void sendEmailDialog() {
